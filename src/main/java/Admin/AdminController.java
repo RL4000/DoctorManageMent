25ec315fa63b03c1f6f5cd259cf775c0596c8033
Ngo Anh Tu <63779177+n-anhtu0101@users.noreply.github.com>
2021-02-02 13:44:04 +0700

    update filenal

--- src/main/java/Admin/AdminController.java
+++ src/main/java/Admin/AdminController.java
@@ -21,23 +21,22 @@ import java.util.Date;
  * @author Admin
  */
 public class AdminController {
-    
+
     Validate validate;
     ValidationAdminManager adminManager;
     UserDataIO userDataIO;
-    
+
     ArrayList<User> listUsers;
     ArrayList<Patient> listPatients;
     Doctor doctorGotByUserCode;
-    
+
     public AdminController() {
         validate = new Validate();
         adminManager = new ValidationAdminManager();
         userDataIO = new UserDataIO();
-        listPatients = new ArrayList<>();
-        listUsers = new ArrayList<>();
+        initMemoryData();
     }
-    
+
     public void processing() throws IOException {
         //--------Đọc data, xóa sau
         initMemoryData();
@@ -48,7 +47,7 @@ public class AdminController {
         listUsers.add(new Doctor("doctor4", "doctor4", UserRole.DOCTOR));
         listUsers.add(new Doctor("5", "doctor5", "doctor5", UserRole.AUTHORIZED_DOCTOR));
         userDataIO.writeDataUser(listUsers);
-        
+
         while (true) {
             listUsers = new ArrayList<>();
             listUsers = userDataIO.readDataUser();
@@ -57,20 +56,20 @@ public class AdminController {
                 if (user.getUserRole() == UserRole.AUTHORIZED_DOCTOR) {
                     System.out.println(user);
                 }
-            }            
+            }
             System.err.println(ConsoleColors.RED + "Size: " + listUsers.size()); //8 element
 
             String usercode = validate.getString("Enter usercode: ");
             doctorGotByUserCode = (Doctor) adminManager.getDoctorByUserCode(usercode, listUsers);
-            
+
             if (doctorGotByUserCode == null) {
                 System.out.println("Doctor is not exist!!!");
                 System.out.println("");
                 continue;
             }
-            
+
             listPatients = doctorGotByUserCode.getPatients();
-            
+
             if (listPatients.isEmpty()) {
                 System.out.println(ConsoleColors.RED + "List patient's this doctor is emty");
             } else {
@@ -81,7 +80,7 @@ public class AdminController {
                 });
                 System.out.println("");
             }
-            
+
             printMENU_AddUpdatePatient();
             int choice = validate.getINT_LIMIT("Enter choice: ", 1, 2);
             switch (choice) {
@@ -103,14 +102,14 @@ public class AdminController {
             break;
         }
     }
-    
+
     private void printMENU_AddUpdatePatient() {
         System.out.println(ConsoleColors.BLUE_BOLD + "-----------------------------------");
         System.out.println(ConsoleColors.BLUE_BOLD + "1\tAdd new a patient");
         System.out.println(ConsoleColors.BLUE_BOLD + "2\tUpdate a patient");
         System.out.println(ConsoleColors.BLUE_BOLD + "-----------------------------------");
     }
-    
+
     private void addNewPatient() throws IOException {
         while (true) {
             int patientid = validate.getINT_LIMIT("Enter patient id: ", 1, Integer.MAX_VALUE);
@@ -120,19 +119,19 @@ public class AdminController {
                 System.out.println("");
                 continue;
             }
-            
+
             String name = validate.getString("Enter name: ");
             String diseaseType = validate.getString("Enter diseaseType: ");
             Date consultDate = validate.getDate_LimitToCurrent("Enter consultDate: ");
             String consultNote = validate.getString("Enter consultNote: ");
-            
+
             listPatients.add(new Patient(patientid, name, diseaseType, consultDate, consultNote));
-            
+
             break;
         }
-        
+
     }
-    
+
     private void updateAPatient() throws IOException {
         while (true) {
             int patientid = validate.getINT_LIMIT("Enter patient id: ", 1, Integer.MAX_VALUE);
@@ -141,12 +140,12 @@ public class AdminController {
                 System.out.println(ConsoleColors.RED + "ID is not exist");
                 continue;
             }
-            
+
             String newName = validate.getString("Enter name: ");
             String newDiseaseType = validate.getString("Enter diseaseType: ");
             Date newConsultDate = validate.getDate_LimitToCurrent("Enter consultDate: ");
             String newConsultNote = validate.getString("Enter consultNote: ");
-            
+
             patient.setName(newName);
             patient.setDiseaseType(newDiseaseType);
             patient.setConsultDate(newConsultDate);
@@ -158,6 +157,7 @@ public class AdminController {
     private void initMemoryData() {
         listUsers = new ArrayList<>();
         listPatients = new ArrayList<>();
+        doctorGotByUserCode = null;
     }
-    
+
 }
